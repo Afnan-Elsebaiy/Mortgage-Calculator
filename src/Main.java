5ae0ec60c2b321ac169eed873a5bafc6e8cea69a
AfnanElsebaiy <afnan.elsebaiy12@gmail.com>
2023-05-31 16:33:29 +0200

    handel error to inputs by user

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,48 @@ public class Main {
         final int PERCENT = 100;
         final int MONTHS_IN_YEAR = 12;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = sc.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        float AnnualInterestRate = sc.nextFloat()/PERCENT/MONTHS_IN_YEAR;
-        System.out.print("Period: ");
-        int periodOfYear = sc.nextInt()*MONTHS_IN_YEAR;
-        float Mortgage =principal*(AnnualInterestRate*(float) Math.pow(1+AnnualInterestRate,periodOfYear))/((float)Math.pow(1+AnnualInterestRate,periodOfYear)-1);
-//        System.out.println("$"+Mortgage);
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(Mortgage);
-        System.out.println(result);
+
+        float AnnualInterestRates;
+        int principal;
+        float AnnualInterestRate;
+        int period;
+
+
+        while (true) {
+            System.out.print("Principal: ");
+            principal = sc.nextInt();
+            if(principal >= 1000 && principal <= 1000000) {
+                break;
+            }
+            System.out.println("Enter a number between 1000 and 1000000");
+        }
+
+                while (true) {
+                    System.out.print("Annual Interest Rate: ");
+                    AnnualInterestRate = sc.nextFloat();
+                    if (AnnualInterestRate >= 1 && AnnualInterestRate <= 30) {
+                        AnnualInterestRates = AnnualInterestRate / PERCENT / MONTHS_IN_YEAR;
+                        break;
+//                        System.out.println(AnnualInterestRate);
+                    }
+                    System.out.println("Enter a value greater than 0 and less than or equal 30");
+
+                }
+                        while (true) {
+                            System.out.print("Period: ");
+                            period = sc.nextInt();
+                            if (period >= 1 && period <= 30) {
+                                 int periodOfYear = period * MONTHS_IN_YEAR;
+//                                System.out.println(periodOfYear);
+                                float Mortgage =principal*(AnnualInterestRates*(float) Math.pow(1+AnnualInterestRates,periodOfYear))/((float)Math.pow(1+AnnualInterestRates,periodOfYear)-1);
+                                NumberFormat currency = NumberFormat.getCurrencyInstance();
+                                String result = currency.format(Mortgage);
+                                System.out.println(result);
+                                break;
+                            }
+                            System.out.println("Enter a value between 1 and  30");
+
+                        }
 
     }
 }
